f0efde739337498fdbd1ffccfc3b191faeaf5a90
Alexey <168357108+ago106@users.noreply.github.com>
2026-02-01 18:50:24 +0700

    Update Scripts.lua

--- Scripts/TDS/Utility/Scripts.lua
+++ Scripts/TDS/Utility/Scripts.lua
@@ -1,21 +1,5 @@
 return {
     {
-        id = "Macro",
-        title = "Macro Recorder",
-        description = "Recording your game.",
-        is_premium = true,
-        pinned = true,
-        image_id = "rbxassetid://87833741091101",
-        script_url = "",
-        details = {
-            lvl = "any",
-            towers = "any",
-            map = "any",
-            mode = "any",
-            difficult = "any"
-        }
-    },
-    {
         id = "AutoGems",
         title = "Auto Gems",
         description = "Hardcore Strat to AFK Grind",
@@ -29,38 +13,6 @@ return {
             map = "Crossroads",
             mode = "Hardcore",
             difficult = "1"
-        }
-    },
-    {
-        id = "AutoMoney",
-        title = "Auto Money",
-        description = "Pizza Party Strat to AFK Grind",
-        is_premium = true,
-        pinned = false,
-        image_id = "rbxassetid://87833741091101",
-        script_url = "",
-        details = {
-            lvl = 30,
-            towers = {"Trapper", "Shotgunner"},
-            map = "Pizza Party",
-            mode = "Normal",
-            difficult = "any"
-        }
-    },
-    {
-        id = "Test",
-        title = "Test",
-        description = " Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test",
-        is_premium = false,
-        pinned = false,
-        image_id = "rbxassetid://87833741091101",
-        script_url = "",
-        details = {
-            lvl = 30,
-            towers = {},
-            map = "awgggggggggggggggggggggggggggggg awg wagggggggggggggggggggg",
-            mode = "Hard",
-            difficult = "any"
         }
     }
 }
