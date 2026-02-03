5e659c13813488f38071629e26f33b380fb23d03
Alexey <168357108+ago106@users.noreply.github.com>
2026-02-03 21:18:08 +0700

    Update Scripts.lua

--- Scripts/TDS/Utility/Scripts.lua
+++ Scripts/TDS/Utility/Scripts.lua
@@ -14,5 +14,21 @@ return {
             mode = "Hardcore",
             difficult = "N/A"
         }
+    },
+    {
+        id = "PizzaParty",
+        title = "Auto Money Free",
+        description = "Hardcore Strat to AFK Grind Gems",
+        is_premium = false,
+        pinned = true,
+        image_id = "rbxassetid://87833741091101",
+        script_url = "https://raw.githubusercontent.com/Space-RB/Premium/refs/heads/main/Scripts/TDS/Scripts/FreePizzaParty.lua",
+        details = {
+            lvl = 50,
+            towers = {"Shotgunner", "Trapper"},
+            map = "Pizza Party",
+            mode = "Special",
+            difficult = "N/A"
+        }
     }
 }
