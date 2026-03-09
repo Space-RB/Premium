7bc96a1e7f0da03f4d5eb3b13874c717fa876bff
Alexey <168357108+ago106@users.noreply.github.com>
2026-03-09 18:33:15 +0700

    Update HWID data - 2026-03-09 16:33

--- DataBase/v3/Trial.lua
+++ DataBase/v3/Trial.lua
@@ -1,4 +1,5 @@
 local TrialKeys = {
  ["95iy5fgpdbfjvj2emho8qqa3r8o82k0c"] = "HWID copied", -- 1262412048025915543 / 7d / 16.03.2026
+ ["tvat69692w2bzbc8nea9822gvxw9oux3"] = "9252E677E68B4BFFE223F4B7DC119D", -- 869595045949481002 / 7d / 16.03.2026
 }
 return TrialKeys;
