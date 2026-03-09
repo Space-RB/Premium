8978fbf6ee7d45649bd44084e002fc9cc2061989
Alexey <168357108+ago106@users.noreply.github.com>
2026-03-09 20:43:48 +0700

    Update HWID data - 2026-03-09 18:43

--- DataBase/v3/Trial.lua
+++ DataBase/v3/Trial.lua
@@ -1,5 +1,4 @@
 local TrialKeys = {
- ["95iy5fgpdbfjvj2emho8qqa3r8o82k0c"] = "455d2dce90d675f6ba495d7075518ea7d4f0ce1bbabeeaf84943aa7684a5659f", -- 1262412048025915543 / 7d / 16.03.2026
- ["tvat69692w2bzbc8nea9822gvxw9oux3"] = "7de36c8090665eb3", -- 869595045949481002 / 7d / 16.03.2026
+ ["63batf5yxw107wkd6sxgcn4l6iy8wyo7"] = "8892006f-fd1f-11f0-b95f-806e6f6e6963", -- 1399001900636045443 / 1d / 10.03.2026
 }
 return TrialKeys;
