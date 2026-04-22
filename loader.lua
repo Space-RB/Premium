8faee853a6d8dc81d319c0cce015e2151d8f62c4
Alexey <168357108+ago106@users.noreply.github.com>
2026-04-22 19:55:45 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,23 +1,24 @@
-repeat task.wait(0.1) until game:IsLoaded();
+repeat task.wait(0.1) until game:IsLoaded()
 print("[Space Hub]: Premium Script Loading...")
 
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
 if TDS then
-    getgenv().GameName = "TDS";
+    getgenv().GameName = "TDS"
 end
-if beta then
-    getgenv().BetaTest = true;
-end
-if game then
-    getgenv().GameName = game;
-end
-if not script_key then
-    LocalPlayer:Kick("Use | script_key=\"\"")
-    warn("[Space Hub]: Not script_key in loader")
-    return;
-end;
 
-getgenv().Key = script_key;
-loadstring(game:HttpGet("https://f.space-hub.cc/premium-loader.lua"))()
+if beta then
+    getgenv().BetaTest = true
+end
+
+local providedKey = tostring(script_key or "")
+if providedKey:gsub("%s+", "") == "" then
+    LocalPlayer:Kick('Use script_key="<your_key>"')
+    warn("[Space Hub]: script_key was not provided")
+    return
+end
+
+getgenv().Key = providedKey
+_G.Key = providedKey
+loadstring(game:HttpGet("https://f.space-hub.cc/Premium.lua"))()
